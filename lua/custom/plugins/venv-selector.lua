b82e0ca921fe652609370a3ee4db852008d78976
lepa22 <lepa.22@hotmail.com>
2024-04-03 12:29:04 +0300

    Set path for Anaconda environment in venv-selector.lua

--- lua/custom/plugins/venv-selector.lua
+++ lua/custom/plugins/venv-selector.lua
@@ -1,10 +1,21 @@
 return {
   'linux-cultist/venv-selector.nvim',
-  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-telescope/telescope.nvim',
+    'mfussenegger/nvim-dap-python',
+  },
   opts = {
     -- Your options go here
     -- name = "venv",
     -- auto_refresh = false
+
+    -- Set path for Anaconda environments
+    anaconda_base_path = '/home/lepa/miniconda3',
+    anaconda_envs_path = '/home/lepa/miniconda3/envs',
+
+    -- Don't search parent directories for venvs
+    parents = 0,
   },
   event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
