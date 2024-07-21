db04438a7b2744bc94f9a0823e3f7443bf1cc034
lepa22 <lepa.22@hotmail.com>
2024-07-21 09:32:09 +0300

    Changed color of indentscope and enabled catppuccin color scheme for more elements

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -10,12 +10,32 @@ return {
       integrations = {
         mini = {
           enabled = true,
-          indentscope_color = '',
+          indentscope_color = 'lavender',
         },
         mason = true,
         neotree = true,
         lsp_trouble = true,
         which_key = true,
+        fidget = true,
+        noice = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { 'italic' },
+            hints = { 'italic' },
+            warnings = { 'italic' },
+            information = { 'italic' },
+          },
+          underlines = {
+            errors = { 'underline' },
+            hints = { 'underline' },
+            warnings = { 'underline' },
+            information = { 'underline' },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
       },
     }
   end,
