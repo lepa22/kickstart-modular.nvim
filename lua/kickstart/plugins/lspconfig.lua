46317ba593c03b968170a1b9e77667a4c0575a0c
lepa22 <lepa.22@hotmail.com>
2024-09-13 19:18:04 +0300

    renamed `tsserver` to `ts_ls`
    https://github.com/neovim/nvim-lspconfig/pull/3232

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -156,8 +156,8 @@ return {
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
         --
         pylsp = {
           settings = {
@@ -202,7 +202,7 @@ return {
           },
         },
         awk_ls = {},
-        tsserver = {},
+        ts_ls = {},
         bashls = {},
       }
 
@@ -227,7 +227,7 @@ return {
         'shellcheck',
         'shfmt',
         'awk_ls',
-        'tsserver',
+        'ts_ls',
         'prettier',
         'html',
         'marksman',
@@ -241,7 +241,7 @@ return {
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
